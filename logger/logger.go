52c1722aac5397abe3feba1cbddc8329c0a0ff91
Britton Hayes <46035482+brittonhayes@users.noreply.github.com>
2021-08-22 21:45:15 -0700

    Added colored logging and log types (#3)
    Co-authored-by: Britton Hayes <brittonhayes@users.noreply.github.com>

--- logger/logger.go
+++ logger/logger.go
@@ -1,29 +1,65 @@
 package logger
 
 import (
+	"fmt"
 	"io"
 	"text/template"
+
+	"github.com/fatih/color"
 )
 
 type Logger struct {
 	Writer io.Writer
 }
 
+type Level int
+
 const (
-	AttackTemplate = "{{.Attacker}} attacked {{.Target}} with {{.Attack}} for {{.Damage}} pts of damage"
-	StatusTemplate = `
-Name: {{.Character}}
-Health: {{.Health}}
-Armor: {{.Armor}}
-`
+	LevelInfo Level = iota
+	LevelChat
+	LevelCombat
+	LevelError
+)
+
+const (
+	TemplateAttack = `{{.Attacker}} attacked {{.Target}} with {{.Attack}} for {{.Damage}} pts of damage`
+	TemplateStatus = `Name={{.Character}} Health={{.Health}} Armor={{.Armor}}`
 )
 
 func NewLogger(writer io.Writer) *Logger {
 	return &Logger{Writer: writer}
 }
 
+func (l Level) String() string {
+	return [...]string{"INFO", "CHAT", "COMBAT", "ERROR"}[l]
+}
+
+func selectPrefix(level Level) string {
+	var prefix string
+	msgFmt := "%-8s "
+	switch level {
+	case LevelInfo:
+		prefix = color.HiBlackString(msgFmt, level.String())
+	case LevelChat:
+		prefix = color.CyanString(msgFmt, level.String())
+	case LevelCombat:
+		prefix = color.RedString(msgFmt, level.String())
+	case LevelError:
+		prefix = color.WhiteString(msgFmt, level.String())
+	default:
+		prefix = fmt.Sprintf(msgFmt, "LOG")
+	}
+	return prefix
+}
+
+func (l *Logger) Log(level Level, msg string) {
+	prefix := selectPrefix(level)
+	fmt.Fprintln(l.Writer, prefix+msg)
+}
+
 func (l *Logger) Attack(attacker string, target string, attack string, damage float64) error {
-	tpl, err := template.New("log").Parse(AttackTemplate)
+	prefix := selectPrefix(LevelCombat)
+	tpl, err := template.New("log").Parse(prefix + TemplateAttack + "\n")
 	if err != nil {
 		return err
 	}
@@ -37,7 +73,8 @@ func (l *Logger) Attack(attacker string, target string, attack string, damage fl
 }
 
 func (l *Logger) Status(character string, health string, armor string) error {
-	tpl, err := template.New("log").Parse(StatusTemplate)
+	prefix := selectPrefix(LevelInfo)
+	tpl, err := template.New("log").Parse(prefix + TemplateStatus + "\n")
 	if err != nil {
 		return err
 	}
